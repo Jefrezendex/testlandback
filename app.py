1e4f47e9987d436dca5b5b73f2d5129334be46d0
Jefrezendex <jhefferson758@gmail.com>
2025-07-29 00:12:01 -0300

    Update app.py
    Ajuste from flask_cors import CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os, requests, pdfkit
 
 app = Flask(__name__)
+CORS(app)
+
 
 WKHTMLTOPDF_PATH = "/usr/bin/wkhtmltopdf"
 pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PATH)
